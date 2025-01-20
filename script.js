6dc37bd944f961d8abdbcd0d530430712bc05e63
hisato-kozuki <167668521+hisato-kozuki@users.noreply.github.com>
2025-01-20 17:15:29 +0900

    updated

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ for(let i = 0; i < 4; i++){
     for(let j = 0; j < 4; j++){
         let a = document.createElement("box");
         a.id=i*4+j;
-        a.style="top: "+6.25*i+"%; left: "+6.25*j+"%; background-color: rgb("+i*16+","+i*16+",0);";
+        a.style="top: "+25*i+"%; left: "+25*j+"%; background-color: rgb("+i*16+","+i*16+",0);";
         document.getElementsByTagName("body")[0].appendChild(a);
     }
 }
