f2d0f6eb4a6704173d30ff075fccda6febf609e1
hisato_kozuki <q66yuan@gmail.com>
2025-02-26 19:10:43 +0900

    これでスマホ対応になったと思われる。

--- script.js
+++ script.js
@@ -1,74 +1,127 @@
 let mouse = {x: 0, y: 0};
+let boxposi = {x: 0, y: 0};
 let dif = {x: 0, y: 0};
 let flag = 0;
 let first = 0;
-let id;
+let box;
 
 for(let i = 0; i < 4; i++){
     for(let j = 0; j < 4; j++){
         let a = document.createElement("box");
         a.id="a"+(i*4+j);
         console.log("id"+a.id);
-        a.style="top: "+200*i+"px; left: "+200*j+"px; background-color: rgb("+i*32+",0,"+j*32+");";
+        a.style="top: "+200*i+"px; left: "+200*j+"px; background-color: rgb("+i*64+",0,"+j*64+");";
         a.addEventListener("mousedown", down);
+        a.addEventListener("mousemove", drag);
         a.addEventListener("mouseup", stop);
-        a.addEventListener("touchstart", down);
-        a.addEventListener("touchmove", drag);
+        a.addEventListener("touchstart", down_s);
+        a.addEventListener("touchmove", drag_s);
         a.addEventListener("touchend", stop);
         document.getElementsByTagName("body")[0].appendChild(a);
     }
 }
-
+/*function logPosition1(event) {
+    event.preventDefault();
+    console.log(event);
+    mouse[0]=event.offsetX/width;
+    mouse[1]=event.offsetY/height;
+    console.log("offsetX: " + event.offsetX);
+    console.log("offsetY: " + event.offsetY);
+    document.getElementById("a").innerText=mouse[0]+" "+mouse[1];
+  }
+  function logPosition2(event) {
+    event.preventDefault();
+    console.log(event);
+    mouse[0]=event.changedTouches[0].pageX/width;
+    mouse[1]=event.changedTouches[0].pageY/height;
+    document.getElementById("a").innerText=mouse[0]+" "+mouse[1];
+  }*/
 document.onmousemove = drag;
 
 function down(event){
+    console.log(event);
     flag = 1;
-    mouse.x = event.clientX, mouse.y = event.clientY;
-    id = document.getElementById("a"+(Math.floor(mouse.x/200)+Math.floor(mouse.y/200)*4));
-    console.log("id x y "+id.style.left.substring(0, id.style.left.length-2)+" "+id.style.top.substring(0, id.style.top.length-2));
+    mouse.x = event.clientX+window.scrollX, mouse.y = event.clientY+window.scrollY;
+    box = document.getElementById(event.srcElement.id);
+    boxposi = detect_boxposi(box);
+    console.log("box x y "+boxposi.x+" "+boxposi.y);
 
-    dif.x = id.style.left.substring(0, id.style.left.length-2) - mouse.x;
-    dif.y = id.style.top.substring(0, id.style.top.length-2) - mouse.y;
+    dif.x = boxposi.x - mouse.x;
+    dif.y = boxposi.y - mouse.y;
 
-    console.log(id);
-    console.log(mouse.x+" "+mouse.y);
-    console.log(dif.x+" "+dif.y);
+    console.log(box);
+    console.log("mouse "+mouse.x+" "+mouse.y);
+    console.log("dif "+dif.x+" "+dif.y);
+}
+
+function down_s(event){
+    console.log(event);
+    flag = 1;
+    mouse.x = event.changedTouches[0].pageX+window.scrollX, mouse.y = event.changedTouches[0].pageY+window.scrollY;
+    box = document.getElementById(event.srcElement.id);
+    boxposi = detect_boxposi(box);
+    console.log("box x y "+boxposi.x+" "+boxposi.y);
+
+    dif.x = boxposi.x - mouse.x;
+    dif.y = boxposi.y - mouse.y;
+
+    console.log(box);
+    console.log("mouse "+mouse.x+" "+mouse.y);
+    console.log("dif "+dif.x+" "+dif.y);
 }
 
 function drag(event){
     if(flag){
         mouse.x = event.clientX, mouse.y = event.clientY;
-        console.log(id);
+        console.log(box);
         console.log(mouse.x+" "+mouse.y);
         console.log(dif.x+" "+dif.y);
-        id.style.left = (mouse.x+dif.x)+"px";
-        id.style.top = (mouse.y+dif.y)+"px";
+        box.style.left = (mouse.x+dif.x+window.scrollX)+"px";
+        box.style.top = (mouse.y+dif.y+window.scrollY)+"px";
+    }
+}
+
+function drag_s(event){
+    if(flag){
+        mouse.x = event.changedTouches[0].pageX, mouse.y = event.changedTouches[0].pageY;
+        console.log(box);
+        console.log(mouse.x+" "+mouse.y);
+        console.log(dif.x+" "+dif.y);
+        box.style.left = (mouse.x+dif.x+window.scrollX)+"px";
+        box.style.top = (mouse.y+dif.y+window.scrollY)+"px";
     }
 }
 
 function stop(){
     flag = 0;
-    let id_ = document.getElementById("a"+(Math.floor((id.style.left.substring(0, id.style.left.length-2)-dif.x)/200)+Math.floor((id.style.top.substring(0, id.style.top.length-2)-dif.y)/200)*4));
-    console.log("id_"+(Math.floor((id.style.left.substring(0, id.style.left.length-2)-dif.x)/200)+Math.floor((id.style.top.substring(0, id.style.top.length-2)-dif.y)/200)*4));
-    //console.log(id_);
-    let buffer = id.id;
-    console.log("buffer:"+buffer+" id_:"+id_.id);
-    console.log(id_);
-    id.id = id_.id; id_.id = buffer;
-    console.log("id:");
-    console.log(id);
-    console.log("id_:");
-    console.log(id_);
-    console.log("id x y"+id.style.left+" "+id.style.top);
+    let box_ = document.getElementById("a"+(Math.floor((box.style.left.substring(0, box.style.left.length-2)-dif.x)/200)+Math.floor((box.style.top.substring(0, box.style.top.length-2)-dif.y)/200)*4));
+    console.log("box_"+(Math.floor((box.style.left.substring(0, box.style.left.length-2)-dif.x)/200)+Math.floor((box.style.top.substring(0, box.style.top.length-2)-dif.y)/200)*4));
+    //console.log(box_);
+    let buffer = box.id;
+    console.log("buffer:"+buffer+" box_:"+box_.id);
+    console.log(box_);
+    box.id = box_.id; box_.id = buffer;
+    console.log("box:");
+    console.log(box);
+    console.log("box_:");
+    console.log(box_);
+    console.log("box x y"+box.style.left+" "+box.style.top);
     for(let i = 0; i < 4; i++){
         for(let j = 0; j < 4; j++){
-            id=document.getElementById("a"+(i*4+j));
-            id.style.left = (Math.floor(Number(id.id.substring(1, id.id.length)))%4*200)+"px";
-            id.style.top = (Math.floor(Number(id.id.substring(1, id.id.length))/4)*200)+"px";
-            id_.style.left = (Math.floor(Number(id_.id.substring(1, id.id.length)))%4*200)+"px";
-            id_.style.top = (Math.floor(Number(id_.id.substring(1, id.id.length))/4)*200)+"px";
+            box=document.getElementById("a"+(i*4+j));
+            box.style.left = (Math.floor(Number(box.id.substring(1, box.id.length)))%4*200)+"px";
+            box.style.top = (Math.floor(Number(box.id.substring(1, box.id.length))/4)*200)+"px";
+            box_.style.left = (Math.floor(Number(box_.id.substring(1, box.id.length)))%4*200)+"px";
+            box_.style.top = (Math.floor(Number(box_.id.substring(1, box.id.length))/4)*200)+"px";
             first = 0;
             console.log("up");
         }
     }
+}
+
+function detect_boxposi(box){
+    let rect = box.getBoundingClientRect();
+    console.log(rect);
+    let boxposi = {x:rect.x+window.scrollX,y:rect.y+window.scrollY}
+    return boxposi;
 }
